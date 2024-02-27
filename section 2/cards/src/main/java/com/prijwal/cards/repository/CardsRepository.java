1f5c06ec34a10140ed18eedb825d68cde128d646
prijwal <prijwal.kushwah@xecurify.com>
2024-02-27 15:52:12 +0530

    cards/accrounts/loans microservices completed

---
+++ section 2/cards/src/main/java/com/prijwal/cards/repository/CardsRepository.java
@@ -0,0 +1,16 @@
+package com.prijwal.cards.repository;
+
+import com.prijwal.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
--- section 2/loans/src/main/java/com/prijwal/loans/LoansApplication.java
+++ section 2/loans/src/main/java/com/prijwal/loans/LoansApplication.java
@@ -1,9 +1,10 @@
-package com.prijwal.accounts;
+package com.prijwal.loans;
 
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,24 +13,27 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @OpenAPIDefinition(
 		info = @Info(
-				title = "Accounts microservice REST API Documentation",
-				description = "Bank Accounts microservice REST API Documentation",
+				title = "Loans microservice REST API Documentation",
+				description = "EazyBank Loans microservice REST API Documentation",
 				version = "v1",
 				contact = @Contact(
-						name = "Prijwal Singh",
-						email = "prijwalrajavat@gmail.com",
-						url = "https://in.linkedin.com/prijwal"
+						name = "Madan Reddy",
+						email = "tutor@eazybytes.com",
+						url = "https://www.eazybytes.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.eazybytes.com"
 				)
 		),
 		externalDocs = @ExternalDocumentation(
-				description =  "Bank Accounts microservice REST API Documentation",
-				url = "https://github.com/prijwal/microservices-project"
+				description = "EazyBank Loans microservice REST API Documentation",
+				url = "https://www.eazybytes.com/swagger-ui.html"
 		)
 )
-public class AccountsApplication {
+public class LoansApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccountsApplication.class, args);
+		SpringApplication.run(LoansApplication.class, args);
 	}
-
 }
