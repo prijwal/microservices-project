d31efdec4f26ceaaeb6d1d51e84b43e1f6539426
prijwal <prijwal.kushwah@xecurify.com>
2024-03-21 06:49:44 +0530

    MySql is connected in all services

---
+++ section7/cards/src/main/java/com/prijwal/cards/CardsApplication.java
@@ -0,0 +1,46 @@
+package com.prijwal.cards;
+
+import com.prijwal.cards.dto.CardsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableConfigurationProperties(value = {CardsContactInfoDto.class})
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "EazyBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Madan Reddy",
+						email = "tutor@eazybytes.com",
+						url = "https://www.eazybytes.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.eazybytes.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Cards microservice REST API Documentation",
+				url = "https://www.eazybytes.com/swagger-ui.html"
+		)
+)
+public class CardsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class);
+	}
+
+}
