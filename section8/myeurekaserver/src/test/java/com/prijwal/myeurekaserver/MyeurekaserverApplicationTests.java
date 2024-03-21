82de45918eff5c20e0981d16ad05ab02b599a9fd
prijwal <prijwal.kushwah@xecurify.com>
2024-03-21 18:55:18 +0530

    added eureka service registry

---
+++ section8/myeurekaserver/src/test/java/com/prijwal/myeurekaserver/MyeurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prijwal.myeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyeurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
